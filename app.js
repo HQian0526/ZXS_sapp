965ea017c20596069e96e4955153f34b5c65c3bc
“HQian0526” <963497073@qq.com>
2021-09-11 16:03:58 +0800

    0911

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 // app.js
 App({
+data:{      
+},
+
   onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
-        console.log("登录成功",res);
-        let loginCode = res.code
-        wx.setStorageSync("loginCode",res.code)
-        if(this.loginCodeCallBack){
-          this.loginCodeCallBack(loginCode)
+        let token = {
+          loginCode:res.code,
+          openId:'',
+          session_key:""
+        };
+        wx.request({
+          url: 'http://localhost:8888/ZXS/Login/getToken',
+          data:{reqMsg:res.code},
+          method:"POST",
+          success:result=>{
+            if(result.data.code==0){
+              console.log("登录成功",result)
+              const tokens = JSON.parse(result.data.msg);
+              token.session_key = tokens.session_key;
+              token.openId = tokens.openid;
+              wx.setStorageSync("token",token);
+            }
+          }
+        })
+
+        //声明回调函数
+        const tokenRes = wx.getStorageSync("token")
+        let tokenId = tokenRes.openid;
+        if(this.tokenIdCallBack){
+          this.tokenIdCallBack(tokenId)
         }
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
   },
   globalData: {
-    userInfo: null
+    tokenRes: null
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,20 +2,21 @@ import Toast from '@vant/weapp/toast/toast';
 const app = getApp();
 Page({
   data: {
+    branchList:[],    //每个分类下的产品
+    productList:[],   //右侧产品列表
     array:[
-      '请选择校区',
-      '广二师——海珠(敬请期待)',
-      '广二师——花都',
-      '华工广州学院'
+      {text:'请选择校区'},
+      {text:'广二师——海珠(敬请期待)'},
+      {text:'广二师——花都'},
+      {text:'华工广州学院'}
     ],
     position:'请选择校区',    //地区
-    show:false,
-    userInfo: {},
+    show:false,           //是否展示地区选择器 
     user:{},    //用户信息，存入缓存
     loginCode:'',  //登录随机号
     loginStatus:false,
     a:1,
-    goodList:['热卖','护肤','数码','生活','餐厨','衣服','学习','游戏','虚拟','零食','创意','其他'],
+    goodList:['热卖','护肤','数码','生活','餐厨','穿搭','学习','虚拟','零食','创意','其他'],
 
     // 导航及商品
     TabCur: 0,
@@ -29,38 +30,39 @@ Page({
     swiperList: [{
       id: 0,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+      url: 'https://img14.360buyimg.com/pop/jfs/t1/193563/34/7920/52949/60c5a92aE2f97a6f2/ccd6615c2ce24a5f.jpg'
     }, {
       id: 1,
         type: 'image',
-        url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+        url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fa.vpimg2.com%2Fupload%2Fmerchandise%2F69563%2FDANZI-6922726988-2.jpg&refer=http%3A%2F%2Fa.vpimg2.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633316889&t=60b95dd8fd1086d7777d774626e82eff',
     }, {
       id: 2,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimgservice2.suning.cn%2Fuimg1%2Fb2c%2Fimage%2F_5Vna-U0iDBykh0REgWpkA.png&refer=http%3A%2F%2Fimgservice2.suning.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633318655&t=e45f5f15f08c86139a29'
     }, {
       id: 3,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201602%2F14%2F20160214100114_BNMdA.thumb.700_0.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633318920&t=b9aaf618'
     }, {
       id: 4,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.alicdn.com%2Fimgextra%2Fi1%2FTB2.aWNAhuTBuNkHFNRXXc9qpXa_%21%212572264631.jpg_400x400.jpg&refer=http%3A%2F%2Fimg.alicdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1633319132&t=ad27ef'
     }, {
       id: 5,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg10.360buyimg.com%2Fn1%2Fjfs%2Ft1912%2F325%2F2139378791%2F396811%2F89d4dedb%2F56f0a0bbNf2984380.jpg&refer=http%3A%2F%2Fimg10.360buyimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=163331'
     }, {
       id: 6,
       type: 'image',
-      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+      url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.tbw-xie.com%2FtuxieJDEwLmFsaWNkbi5jb20vaTEvMTcxMTYxMTAvVEIyaTdiUFg1d0lMMUpqU1pGc1hYY1hGRlhhXyEhMTcxMTYxMTAkOQ.jpg&refer=http%3A%2F%2Fwww.tbw-xie.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpe'
     }],
   },
   
   onLoad() {
-    if(!app.globalData.loginCode || app.globalData.loginCode==""){  //回调函数确保app.js执行完才执行这个onload
-      app.loginCodeCallBack=loginCode=>{
-        if(loginCode!=""){
+    if(!app.globalData.tokenId || app.globalData.tokenId==""){  //回调函数确保app.js执行完才执行这个onload
+      app.tokenIdCallBack=tokenId=>{
+        if(tokenId!=""){
+          const token = wx.getStorageSync("token");
           const user = wx.getStorageSync("user");
           const school = wx.getStorageSync("school");
           if(!school || ""==school){
@@ -68,6 +70,7 @@ Page({
             this.setData({ show:true })
           }else{
             this.setData({position:school})
+            this.queryProduct();
           }
           if(!user || user==""){  //无缓存，未登录
             this.setData({ loginStatus:false})
@@ -88,7 +91,12 @@ Page({
             })
           }
     }
+    
+    this.initColor();
+  },
 
+  //初始化框架
+  initColor(){
     // 商品及导航
     wx.showLoading({
       title: '加载中...',
@@ -99,18 +107,47 @@ Page({
       list[i] = {};
       list[i].name = this.data.goodList[i];
       list[i].id = i;
+      list[i].products = [];
     }
     this.setData({
       list: list,
       listCur: list[0]
     })
-
     // 轮播
     this.towerSwiper('swiperList'); // 初始化towerSwiper 传已有的数组名即可
   },
 
   onReady(){
     wx.hideLoading()
+  },
+
+  // 展示商品
+  queryProduct(){
+    let that = this;
+    wx.request({
+      url: 'http://localhost:8888/ZXS/product/getProductList',
+      data:{reqMsg:this.data.position},
+      method:"POST",
+      success:res=>{
+        console.log("res",res);
+        that.setData({
+          productList:res.data
+        })
+        for(var a = 1;a<that.data.list.length;a++){
+          for(var i = 0; i<res.data.length;i++){
+            if(that.data.list[a].name == res.data[i].productType){
+              that.data.list[a].products.push(res.data[i])
+            }
+          }
+        }
+        this.setData({
+          list:that.data.list
+        })
+      },
+      fail:res=>{
+        console.log(res)
+      }
+    })
   },
 
   /**
@@ -175,14 +212,15 @@ Page({
       })
     }
     this.setData({ show:false})
+
   },
 
   //选择器确认
   onConfirm(event){
     const { picker, value, index } = event.detail;
-    wx.setStorageSync("school",value);
+    wx.setStorageSync("school",value.text);
     this.setData({
-      position:value,
+      position:value.text,
       show:false
     })
     if(index==0){
@@ -190,7 +228,9 @@ Page({
         title: '因未选择校区，首页所展示的数据为模拟数据，望悉知',
         icon:'none'
       })
+      return
     }
+    this.queryProduct();
   },
 
   // 商品导航栏--
